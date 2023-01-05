f95c041e0b3e344fa24ad72d8e9c2c09c5ab0d09
nezhiliaev <nezhiliaev@hex.team>
2023-01-05 15:18:51 +0300

    - Added Hilt - added signingConfigs to app's build.gradle

---
+++ app/src/main/java/com/reaper/netexplorer/BaseApplication.kt
@@ -0,0 +1,8 @@
+package com.reaper.netexplorer
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class BaseApplication : Application() {
+}
--- app/src/main/java/com/reaper/netexplorer/presentation/MainActivity.kt
+++ app/src/main/java/com/reaper/netexplorer/presentation/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.reaper.netexplorer
+package com.reaper.netexplorer.presentation
 
 import android.os.Bundle
 import android.view.Menu
@@ -11,6 +11,7 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
+import com.reaper.netexplorer.R
 import com.reaper.netexplorer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
